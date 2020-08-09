c7cd6c6f5450ea66400fd334ada6a9163369a6e2
tatsuya.oishi <chooch0203@gmail.com>
2020-08-10 02:06:24 +0900

    Add Employee new

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -1,19 +1,23 @@
 package controllers.employees;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+
 /**
  * Servlet implementation class EmployeesNewServlet
  */
 @WebServlet("/employees/new")
 public class EmployeesNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +26,15 @@ public class EmployeesNewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("employee", new Employee());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+        rd.forward(request, response);
+    }
 
 }
